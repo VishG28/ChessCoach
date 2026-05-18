54e0fc4903eae8d9f16b359812c63539ae7676d7
Vishruth Gaur <vish28.gaur@gmail.com>
2026-05-17 21:11:53 -0700

    feat(board): wrap router with BoardThemeProvider and PieceSetProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { ApiKeyProvider } from '@/coaching/apiKey'
 import { CostCounterProvider } from '@/coaching/costCounter'
+import { BoardThemeProvider } from '@/components/board/BoardThemeProvider'
+import { PieceSetProvider } from '@/components/board/PieceSetProvider'
 import { ErrorBoundary } from './components/ErrorBoundary'
 import { router } from './router'
 import './index.css'
@@ -12,7 +14,11 @@ createRoot(document.getElementById('root')!).render(
     <ErrorBoundary>
       <ApiKeyProvider>
         <CostCounterProvider>
-          <RouterProvider router={router} />
+          <BoardThemeProvider>
+            <PieceSetProvider>
+              <RouterProvider router={router} />
+            </PieceSetProvider>
+          </BoardThemeProvider>
         </CostCounterProvider>
       </ApiKeyProvider>
     </ErrorBoundary>
